5319eb0240e7d1a911e724a343e69eb85748290d
Timbrowne111 <browne.t@buas.nl>
2026-07-27 17:56:56 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1-XG25ESmI97gHYYKwK9Zb4PUvjW5O6A",
-  authDomain: "shiftmaster-de483.firebaseapp.com",
-  databaseURL: "https://shiftmaster-de483-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "shiftmaster-de483",
-  storageBucket: "shiftmaster-de483.firebasestorage.app",
-  messagingSenderId: "497684359968",
-  appId: "1:497684359968:web:092384543a44de6fdb4545",
-  measurementId: "G-K5SL7Y2MR0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
